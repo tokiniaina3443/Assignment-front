bab9a3ef3f3d3cb22bc4b91435767d3ea2c40d44
Andomahefa <andomahefa.julio.r@gmail.com>
2023-06-20 22:05:51 +0300

    rendre

--- src/app/assignments/list-assignment/rendre/rendre.component.ts
+++ src/app/assignments/list-assignment/rendre/rendre.component.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AssignmentsService } from 'src/app/shared/assignments.service';
+import { Assignment } from '../../assignment.model';
 
 @Component({
   selector: 'app-rendre',
   templateUrl: './rendre.component.html',
   styleUrls: ['./rendre.component.css'],
 })
-export class RendreComponent {}
+export class RendreComponent implements OnInit {
+  assignment: Assignment | undefined;
+  noteAssignment: number | undefined;
+  remarqueAssignment: string | undefined;
+
+  constructor(
+    private assignmentsService: AssignmentsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.getAssignment();
+  }
+
+  getAssignment() {
+    const id = +this.route.snapshot.params['id'];
+
+    this.assignmentsService.getAssignment(id).subscribe((assignment) => {
+      if (!assignment) return;
+
+      this.assignment = assignment;
+    });
+  }
+
+  onRendreAssignment() {
+    if (!this.assignment || !this.assignment._id || (!this.noteAssignment)|| (!this.remarqueAssignment)) return;
+  
+    this.assignmentsService
+      .rendreAssignment(this.assignment._id, this.noteAssignment, this.remarqueAssignment)
+      .subscribe(() => {
+        // Handle the successful update here
+        console.log('Assignment rendu successfully');
+  
+        // Redirect to the appropriate page or perform any other action
+        // For example, navigate to the home page
+        this.router.navigate(['']);
+      }, (error) => {
+        // Handle the error here
+        console.log('Error rendu assignment:', error);
+  
+        // Show an error message or perform any other action
+      });
+  }
+  
+}
