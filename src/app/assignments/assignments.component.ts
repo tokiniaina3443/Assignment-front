75cb3a44a95dda525c3ccead5639000492eb8de2
Tokiniaina <randrianarimananatokiniaina47@gmail.com>
2023-06-20 22:16:27 +0300

    logout

--- src/app/assignments/assignments.component.ts
+++ src/app/assignments/assignments.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AssignmentsService } from '../shared/assignments.service';
+import { LocalStorageService } from '../services/local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-assignments',
@@ -9,7 +11,16 @@ import { AssignmentsService } from '../shared/assignments.service';
 export class AssignmentsComponent implements OnInit {
   showFiller: boolean = false;
   features = ['Hydrodynamic', 'Port & Starboard Attachments', 'Turbo Drive'];
-  constructor(private assignmentsService: AssignmentsService) {}
+  constructor(
+    private assignmentsService: AssignmentsService,
+    private localStorage: LocalStorageService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
+
+  logOut() {
+    this.localStorage.remove('token');
+    this.router.navigate(['/login']);
+  }
 }
